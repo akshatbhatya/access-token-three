36dd2ab8505732974a5933c48c200cb8aee1a024
akshat <akshatbhatya@gmail.com>
2024-04-17 17:18:31 +0530

    add routes to app file

--- src/app.js
+++ src/app.js
@@ -1,30 +1,35 @@
-import cookieParser from "cookie-parser"
-import express from "express"
-import cors from "cors"
-const app = express()
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.use(
   express.urlencoded({
     limit: "16kb",
-    extended:true
+    extended: true,
   })
-)
+);
 
 app.use(
   express.json({
     limit: "16kb",
   })
-)
+);
 
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     Credential: true,
   })
-)
+);
 
-app.use(cookieParser())
+app.use(cookieParser());
+// routes
+
+import router from "./routes/user.routes.js";
+
+app.use("/api/v1/users", router);
 
 export default app;
