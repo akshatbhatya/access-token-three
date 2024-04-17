68e1aec42452c546e247cf26217eca9d8d6fbf1d
akshat <akshatbhatya@gmail.com>
2024-04-17 15:11:40 +0530

    index file is done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import dotenv from "dotenv"
+import connectDb from "./db/index.js";
+import app from "./app.js";
+import dotenv from "dotenv";
 dotenv.config({
   path: "./.env",
-})
-
-
+});
+const portNo = process.env.PORT || 8080;
+connectDb()
+  .then(() => {
+    app.listen(portNo, () => {
+      console.log(`http://localhost:${portNo}`);
+    });
+  })
+  .catch((err) => console.log(err || "database connectoion is unsucessfull"));
