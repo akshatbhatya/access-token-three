6f0fc90dae9fac50348c932bb4e11daf25f53f53
akshat <akshatbhatya@gmail.com>
2024-04-17 15:08:00 +0530

    add comas

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,15 @@
-import mongoose from "mongoose"
-import DataBaseName from "../constants.js"
+import mongoose from "mongoose";
+import DataBaseName from "../constants.js";
 
 const connectDb = async () => {
   try {
-    const database = await mongoose.connect(`${process}/${DataBaseName}`)
-    const response = database.Connection.host
-    console.log(response)
-    return response
-
+    const database = await mongoose.connect(`${process}/${DataBaseName}`);
+    const response = database.Connection.host;
+    console.log(response);
+    return response;
   } catch (error) {
-    return error
+    return error;
   }
-}
+};
 
 export default connectDb;
