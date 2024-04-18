b4b25cad1b4dd49dcebf56901b71db75c71e1b33
akshat <akshatbhatya@gmail.com>
2024-04-18 09:32:31 +0530

    user controller works

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,8 @@
 import asyncHandler from "../utils/asyncHandller.js";
 
-const register = asyncHandler(async (req, res) => {});
+const register = asyncHandler(async (req, res) => {
+  const { username, email, password, avtar, coverAvtar } = req.body;
+  console.log(username);
+});
+
+export { register };
